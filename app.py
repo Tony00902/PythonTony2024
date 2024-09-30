aad71618b5568967065fc0a4fdfb9e91f9064158
tony <wangzhenweitt@gmail.com>
2024-09-30 18:01:04 -0400

    tony

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
+app = Flask (__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "HOME"
+
+
+
+
+client = MongoClient('mongodb+srv://wangzhenweitt:rdtF1YZHIhsP7kfj@cluster0.jeium.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client['PYTHONTONY2024']
+users_collection = db['Users']
+
+
+print(users_collection)
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users= []
+    for user in users_collection.find():
+        user['_id'] = str(suer['_id'])
+        users.append(user)
+    return  jsonify(users)
+
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
